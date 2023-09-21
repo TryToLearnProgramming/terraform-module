b9c50cc860eccc2af95eeb66e60783f19d0f8af5
devops-sami <sami@codelogicx.com>
2023-09-21 19:11:36 +0530

    vpc module created

---
+++ modules/vpc/variables.tf
@@ -0,0 +1,25 @@
+# modules/vpc_dem/variables.tf
+
+variable "name" {
+  description = "CIDR block for the VPC"
+  type = string
+}
+
+variable "vpc_cidr_block" {
+  description = "CIDR block for the VPC"
+}
+
+variable "public_subnet_cidr_blocks" {
+  description = "List of Public CIDR blocks for subnets in the VPC"
+  type        = list(string)
+}
+
+variable "private_subnet_cidr_blocks" {
+  description = "List of Private CIDR blocks for subnets in the VPC"
+  type        = list(string)
+}
+
+variable "availability_zones" {
+  description = "List of Availability Zones for subnets"
+  type        = list(string)
+}
